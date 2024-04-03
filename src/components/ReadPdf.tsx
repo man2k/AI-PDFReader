678d0fb15e43de7486d87220db6fda78d4a42a06
man2k <mankarmakar4@gmail.com>
2024-04-04 04:37:23 +0530

    Added pdf text extraction functionality, added text to speech functionality, minor UI changes for reading and audio

--- src/components/ReadPdf.tsx
+++ src/components/ReadPdf.tsx
@@ -1,18 +1,35 @@
 import { invoke } from "@tauri-apps/api";
 import { useEffect, useState } from "react";
 
+let path = "";
 const ReadPdf = () => {
-  let path = "";
   useEffect(() => {
     path = JSON.parse(sessionStorage.getItem("path") as string).absPath;
     console.log("readpdf front", path);
+  }, []);
+  const readHandler = () => {
+    console.log(path);
+    const k = document.getElementById("readBtn");
+    if (k) k.textContent = "Reading..";
     invoke("readpdf", { path: path }).then((e) => {
       console.log(e);
+      if (k) k.textContent = "Start Reading";
     });
-  }, []);
-
+  };
   const [parsedPdf, setParsedPdf] = useState<string>();
-  return <div>{parsedPdf}</div>;
+  return (
+    <div>
+      <div>
+        <button
+          className="border-2 rounded-lg px-2 border-amber-400 bg-amber-800 hover:bg-amber-900"
+          onClick={readHandler}
+          id="readBtn"
+        >
+          Start Reading
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ReadPdf;
--- src/pages/PdfDisplay.tsx
+++ src/pages/PdfDisplay.tsx
@@ -4,16 +4,18 @@ import ReadPdf from "../components/ReadPdf";
 const PdfDisplay = () => {
   const [pdfFilePath, setPdfFilePath] = useState<string>("");
   useEffect(() => {
+    const a = document.getElementById("pdfViewer");
+    console.log(a);
     const pp = JSON.parse(sessionStorage.getItem("path") as string);
     setPdfFilePath(pp.webPath as string);
   }, []);
 
   return (
-    <main className="w-screen h-screen flex flex-col items-center justify-center border-2 border-white bg-white">
+    <main className="w-screen h-screen flex flex-col items-center justify-start border-2 border-white bg-black text-white">
       <div className="text-white">
         <iframe
           src={pdfFilePath}
-          className="w-screen h-screen"
+          className="w-screen h-[570px] border-2 border-amber-500"
           id="pdfViewer"
         />
       </div>
